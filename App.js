3669bf1282fb66362cad87ebcdf68fff38d25a03
Reinaldo Trindade <rewifetri@gmail.com>
2020-03-29 20:28:46 -0300

    Added icons on tab navigator

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+
 import Home from "./Home";
 
 // const instructions = Platform.select({
@@ -12,7 +14,7 @@ import Home from "./Home";
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      {/* <FontAwesome name="search" size={32} color="lightcoral"/> */}
     </View>
   );
 }
@@ -22,9 +24,30 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Search') {
+              iconName = focused
+                ? 'search'
+                : 'search';
+            } else if (route.name === 'My Pokémon') {
+              iconName = focused ? 'pets' : 'pets';
+            }
+
+            // You can return any component that you like here!
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'lightcoral',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Search" component={Home} />
+        <Tab.Screen name="My Pokémon" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
